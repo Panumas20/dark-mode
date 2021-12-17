b6b7f4cea00d0305e47f6dd27485b603528fcc94
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2021-12-17 17:54:24 +0700

    Final Code

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,16 @@
 import light from '../image/light.svg';
 import Dark from '../image/dark.svg';
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
 const Content = () => {
+	const { theme } = useContext(ThemeContext);
 	return (
-		<main>
+		<main className={theme}>
 			<div>
-				<h1>Panumas Student</h1>
+				<h1>Student First</h1>
 				<p>Darkmode Workshop</p>
 			</div>
-			<img src={light} alt="logo" />
+			<img src={theme === 'dark' ? Dark : light} alt="logo" />
 		</main>
 	);
 };
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,16 @@
+import { useContext, useState } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from '../App';
+
 const Title = () => {
+	const { theme, setTheme } = useContext(ThemeContext);
+	const toggleSwitch = (checked) => {
+		setTheme(theme === 'light' ? 'dark' : 'light');
+	};
 	return (
-		<header>
-			<span>Mode</span>
+		<header className={theme}>
+			<span>Mode [{theme}]</span>
+			<Switch onChange={toggleSwitch} checked={theme === 'dark'} checkedIcon={false} uncheckedIcon={false} onColor={'#ffa500'} />
 		</header>
 	);
 };
